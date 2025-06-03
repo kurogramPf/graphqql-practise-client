7f87f2fd2ddcfc2cebb25f6e027bfd2d88bb7eb8
kurogram0-r <kurogram0.r@gmail.com>
2025-06-03 16:44:20 +0900

    GraphQL利用前Client実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import { Button } from "./components/ui/button";
 import { cn } from "@/lib/utils";
+import { Label } from "./components/ui/label";
+import { Checkbox } from "./components/ui/checkbox";
+
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
+  const todos = [
+    { id: "1", title: "GraphQLを学ぶ", completed: false },
+    { id: "2", title: "Reactを学ぶ", completed: false },
+  ] as Todo[];
+
   return (
-    <>
-      <Button className={cn("")}>ボタン</Button>
-    </>
+    <div>
+      <h1>ToDo List</h1>
+      <input type="text" placeholder="ToDoを追加してください。" />
+      <Button className={cn("bg-blue-500 text-white")}>追加</Button>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className={cn("flex", todo.completed && "line-through")}
+          >
+            <Label htmlFor={`item_${todo.id}`}>
+              <Checkbox checked={todo.completed} id={`item_${todo.id}`} />
+              {todo.title}
+            </Label>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
